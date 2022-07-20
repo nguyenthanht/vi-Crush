8035b132719560a943325e737631cca8fb59a08d
nguyenthanht <86815188+nguyenthanht@users.noreply.github.com>
2022-07-20 18:45:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,5 +16,5 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'không chịu cũng chịu',
     btnAccept: 'Okiiiii lun .<3',
-    messLink: //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: //link mess của các bạn.
 }
